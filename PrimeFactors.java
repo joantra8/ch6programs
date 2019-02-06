f398ef7c948956171e48a7185590b2f30568844b
joantra8 <45926552+joantra8@users.noreply.github.com>
2019-02-06 05:21:44 -0500

    Update PrimeFactors.java

--- PrimeFactors.java
+++ PrimeFactors.java
@@ -1,5 +1,27 @@
 package joans_workspace;
-
+import java.util.Scanner;
 public class PrimeFactors {
+	public static void main (String []args) {
+		Scanner input = new Scanner(System.in);
+		int number;
+		int counter = 2;
+		
+		System.out.print("Enter a number: ");
+		number = input.nextInt();
+		
+		while (counter <= number) {
+			if (number % counter == 0) {
+				System.out.println(counter);
+				number = number / counter;
+			} else {
+				counter += 1;
+				
+				input.close();
+				
+			}
+		}
+	
+		
+	}
 
 }
