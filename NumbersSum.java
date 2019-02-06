aa7db528900b93bfc45ab84a0351f166cf83ff5c
joantra8 <45926552+joantra8@users.noreply.github.com>
2019-02-06 05:20:27 -0500

    Update NumbersSum.java

--- NumbersSum.java
+++ NumbersSum.java
@@ -1,5 +1,24 @@
 package joans_workspace;
-
+import java.util.Scanner;
 public class NumbersSum {
+	public static void main (String []args) {
+		Scanner input = new Scanner(System.in);
+		int compnum = 1;
+		int sumcomp = 0;
+		int usernum;
+		System.out.println("Please enter a number greater than 1: ");
+		usernum = input.nextInt();
+		
+		while (compnum <= usernum) {
+			System.out.print(compnum);
+			sumcomp += compnum;
+			compnum += 1;
+		}
+		System.out.print(sumcomp);
+		
+		input.close();
+		
+	}
 
 }
+
