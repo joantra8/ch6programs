8cab8278e8583e3bc3e1206f1976adb8d611ab33
joantra8 <45926552+joantra8@users.noreply.github.com>
2019-02-06 05:21:13 -0500

    Update PercentPassing.java

--- PercentPassing.java
+++ PercentPassing.java
@@ -1,5 +1,27 @@
 package joans_workspace;
-
+import java.util.Scanner;
 public class PercentPassing {
+	public static void main (String []args) {
+		Scanner input = new Scanner(System.in);
+		final int SENTINAL = -1;
+		int userscore = 0;
+		int numvalues = 0;
+		int numPassing = 0;
+		double percentPassing;
+		
+		while (userscore != SENTINAL) {
+			System.out.print("Enter your score: ("+ SENTINAL +"to quit)");
+			userscore = input.nextInt();
+			
+			if (userscore > 70) {
+				numPassing += 1;
+				
+			}
+			numvalues += 1;
+		}
+		percentPassing = (double)numPassing/((double) numvalues - 1) * 100;
+		System.out.print("The average of your passing scores is: " + percentPassing + "%");
+		input.close();
+	}
 
 }
