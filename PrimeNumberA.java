e444c154c4048a2bdd998e9dd13fe1195dbebd97
joantra8 <45926552+joantra8@users.noreply.github.com>
2019-02-06 05:22:26 -0500

    Update PrimeNumberA.java

--- PrimeNumberA.java
+++ PrimeNumberA.java
@@ -1,5 +1,38 @@
 package joans_workspace;
-
+import java.util.Scanner;
 public class PrimeNumberA {
+	public static void main (String []args) {
+		Scanner input = new Scanner(System.in);
+		int divide = 1;
+		int integer;
+		int remainder;
+		boolean prime;
+		
+		System.out.println("Enter a whole number greater than 1: ");
+		integer = input.nextInt();
+		
+		do {
+			divide += 1;
+			remainder = (integer % divide);
+			prime = true;
+			if (remainder == 0) {
+				prime = false;
+			}
+			
+		} while ((divide + 1) < integer && remainder != 0);
+		if (prime == false) {
+			System.out.println("The number is not prime.");
+		} else if (prime == true) {
+			System.out.println("The number is prime.");
+			
+			input.close();
+			
+		}
+		
+		
+		
+	
+	}
 
 }
+
