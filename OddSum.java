a80db8dfb8b795a9c2e610b13f948daa5aded66f
joantra8 <45926552+joantra8@users.noreply.github.com>
2019-02-21 07:56:01 -0500

    Update OddSum.java

--- OddSum.java
+++ OddSum.java
@@ -1,5 +1,26 @@
 package joans_workspace;
-
+import java.util.Scanner;
+/* Joan Tra
+ * Period 1
+ * Prompts the user for a number and then takes the sum of all numbers up until user-entered value.
+ */
 public class OddSum {
+	public static void main (String []args) {
+		Scanner input = new Scanner(System.in);
+		int userinput;
+		int finalvalue = 1;
+		
+		System.out.print("Enter an integer: ");
+		userinput = input.nextInt();
+		
+		for (int i = 1; i <= userinput; i += 2) {
+		finalvalue += 1;
+		}
+		
+		System.out.print("The sum is equal to: " + (finalvalue - 1));
+		
+		input.close();
+		
+	}
 
 }
