142d5a7df82272f72ed6eb58652df313fe54d4f8
joantra8 <45926552+joantra8@users.noreply.github.com>
2019-02-21 07:56:51 -0500

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,5 +1,25 @@
 package joans_workspace;
-
+import java.util.Scanner;
+/* Joan Tra
+ * Period 1
+ * Prompts the user for a number and then displays the factorial.
+ */
 public class Factorial {
+	public static void main (String []args) {
+		Scanner input = new Scanner(System.in);
+		int usernum;
+		int factorial = 1;
+		
+		System.out.print("Enter a number: ");
+		usernum = input.nextInt();
+		
+		for (int i = 1; i <= usernum; i++) {
+			factorial = factorial * i;
+			}
+		System.out.print("The factorial of your number is: " + factorial);
+		
+		input.close();
+	}
 
 }
+
