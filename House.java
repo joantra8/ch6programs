8c92468c22564171e3292bc000bf6fce3fe54c40
joantra8 <45926552+joantra8@users.noreply.github.com>
2019-04-01 22:48:25 -0400

    Update House.java

--- House.java
+++ House.java
@@ -1,5 +1,29 @@
 package q4;
 
 public class House {
+	public static void addRoof() {
+		System.out.println("  /\\  ");
+		System.out.println("  / \\  ");
+		System.out.println(" /   \\  ");
+		System.out.println("/_______\\");
+	}
+	public static void addBase() {
+		System.out.println("|   |");
+		System.out.println("|   |");
+		System.out.println("|   |");
+		System.out.println("|______|");
+		
+	}
+	public static void addWalk() {
+		System.out.println("  **");
+		System.out.println("  ***********");
+		
+	}
+	public static void main(String []args) {
+		addRoof();
+		addBase();
+		addWalk();
+	}
+
 
 }
