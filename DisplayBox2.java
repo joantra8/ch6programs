c7448c4bbac38bd04f30168edb14adbd2364d7bd
joantra8 <45926552+joantra8@users.noreply.github.com>
2019-04-01 22:47:05 -0400

    Update DisplayBox2.java

--- DisplayBox2.java
+++ DisplayBox2.java
@@ -1,5 +1,56 @@
-package q4;
-
+import java.util.Scanner;
 public class DisplayBox2 {
-
+	public static void drawBox(int width) {
+		for (int i = 1; i <= width; i++) {
+			System.out.print("*");
+		}
+	}
+	
+	public static void drawBox(int width, String choice) {
+		for (int i = 1; i <= width; i++) {
+			System.out.print(choice);
+		}
+	}
+	public static void drawBox(int height, int width) {
+		for (int i = 1; i <= height; i++) {
+			drawBox(width);
+			System.out.print("");
+		
+		}
+	}
+	public static void drawBox(int height, int width, String choice) {
+		for (int i = 1; i <= height; i++) {
+			drawBox(width, choice);
+			System.out.print("");
+			
+		}
+	}
+	public static void main(String []args) {
+		Scanner input = new Scanner(System.in);
+		int width;
+		int height;
+		
+		String answer;
+		String choice;
+		
+		System.out.print("Enter the width: ");
+		width = input.nextInt();
+		System.out.print("Enter the height: ");
+		height = input.nextInt();
+		System.out.print("Would you like to choose a character? Enter y for yes or n for no.");
+		answer = input.next();
+		
+		if (answer.equals("y")) {
+			System.out.print("Enter your chosen character: ");
+			choice = input.next();
+			
+			drawBox(height, width, choice);
+			
+		} else {
+			drawBox(height, width);
+		
+			
+		}
+		input.close();
+	}
 }
